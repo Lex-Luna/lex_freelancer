7e414268e63d13f7bd1c1c1bb8c97969dcf97a92
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-08-17 17:22:30 -0500

    add MenuCarrusel

--- js/main.js
+++ js/main.js
@@ -18,27 +18,31 @@ const savePregunta = (nombre, telefono, mail, mensaje) =>
         mail,
         mensaje
     })
-formulario.addEventListener("submit", async(e) => {
-        e.preventDefault();
-        const nombre = formulario["Nombre"];
-        const telefono = formulario["Telefono"];
-        const mail = formulario["Mail"];
-        const mensaje = formulario["Mensaje"];
-        try {
-            await savePregunta(nombre.value, telefono.value, mail.value, mensaje.value);
-            formulario.reset();
-            alert('La información ha sido enviada correctamente');
-            nombre.focus();
-        } catch (error) {
-            alert("No ha sido posible enviar los datos");
-        }
-    })
+    /* formulario.addEventListener("submit", async(e) => {
+            e.preventDefault();
+            const nombre = formulario["Nombre"];
+            const telefono = formulario["Telefono"];
+            const mail = formulario["Mail"];
+            const mensaje = formulario["Mensaje"];
+            try {
+                await savePregunta(nombre.value, telefono.value, mail.value, mensaje.value);
+                formulario.reset();
+                alert('La información ha sido enviada correctamente');
+                nombre.focus();
+            } catch (error) {
+                alert("No ha sido posible enviar los datos");
+            }
+        }) */
     // Obtén el botón dropdown
-var dropdown = document.querySelector('.dropdown');
-// Agrega un evento de clic al botón dropdown
-dropdown.addEventListener('click', function(event) {
-    // Obtén el contenido del dropdown
-    var dropdownContent = this.querySelector('.dropdown-content');
-    // Alterna la visibilidad del contenido del dropdown
-    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
-});
+    /* var dropdown = document.querySelector('.dropdown'); */
+    // Agrega un evento de clic al botón dropdown
+    /* dropdown.addEventListener('click', function(event) {
+        // Obtén el contenido del dropdown
+        var dropdownContent = this.querySelector('.dropdown-content');
+        // Alterna la visibilidad del contenido del dropdown
+        dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    }); */
+    // Inicializa el carrusel
+    /* $(document).ready(function() {
+        $('#carouselExampleSlidesOnly').carousel();
+    }); */
